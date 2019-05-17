037849112b51e4c1af7bbde46afdf6b8dc2e69b3
unknown <gamars80@gmail.com>
2019-05-17 13:39:45 +0900

    ddsds

---
+++ src/views/Users.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>이유저의 현재 아이디는 {{userId}}</h1>
+       
+    </div>
+</template>
+
+<script>
+export default{
+    computed:{
+        userId(){
+            //return this.$route.params
+            return this.$route.params.userId
+        }
+    }
+}
+
+</script>
